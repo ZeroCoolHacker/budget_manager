f05dfd6c9a81c74841f404bb6c0552a868af5930
Zero Cool <shahzaibakash@gmail.com>
2019-03-23 17:45:06 +0500

    fixed Board model

--- ledger/models.py
+++ ledger/models.py
@@ -4,10 +4,10 @@ from django.utils.text import slugify
 
 
 # Create your models here.
-class Project(models.Model):
-    name = forms.CharField('Name', max_length=100, required=True)
+class Board(models.Model):
+    name = models.CharField('Name', max_length=100, blank=False)
     slug = models.SlugField(max_length=100, unique=True, blank=True)
-
+    description = models.TextField()
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super().save(*args, **kwargs)
