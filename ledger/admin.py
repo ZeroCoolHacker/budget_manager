33b1a6cc789817d677cfee4da377de00c0cbeb74
Zero Cool <shahzaibakash@gmail.com>
2019-03-23 18:19:10 +0500

    add models to admin

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from ledger.models import Board
+from ledger.models import Board, Category, Transaction
 
 # Register your models here.
 admin.site.register(Board)
+admin.site.register(Category)
+admin.site.register(Transaction)
