959003f6454eba4272a0da0b52e80781136c1416
Zero Cool <shahzaibakash@gmail.com>
2019-03-23 16:36:33 +0500

    Add projects Model

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.text import slugify
+
 
 # Create your models here.
+class Project(models.Model):
+    name = forms.CharField('Name', max_length=100, required=True)
+    slug = models.SlugField(max_length=100, unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+class Transaction(models.Model):
+    """
+    Transactions model
+    It  manages the transactions of company to different users
+    """
+    pass
