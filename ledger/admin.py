c5fa61b370d46ede0c637dbef2a26e3a6c57ebbd
Zero Cool <shahzaibakash@gmail.com>
2019-03-23 18:38:53 +0500

    add custom admin views for category and transaction

--- ledger/admin.py
+++ ledger/admin.py
@@ -3,5 +3,21 @@ from ledger.models import Board, Category, Transaction
 
 # Register your models here.
 admin.site.register(Board)
-admin.site.register(Category)
-admin.site.register(Transaction)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    '''Admin View for Category'''
+
+    list_display = ('board', 'name')
+    list_filter = ('board',)
+    search_fields = ('name',)
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    '''Admin View for Transaction'''
+
+    list_display = ('title', 'amount', 'category', 'transaction_type')
+    list_filter = ('category', 'transaction_type')
+    search_fields = ('title', 'category', 'amount')
