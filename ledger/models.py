d7ace4507808670918c6bf6aeb65ca589b7a12a6
Zero Cool <shahzaibakash@gmail.com>
2019-03-23 18:18:58 +0500

    create Category, Transaction models

--- ledger/models.py
+++ ledger/models.py
@@ -3,6 +3,11 @@ from django.contrib.auth import get_user_model
 from django.utils.text import slugify
 
 
+TRANSACTION_TYPES = (
+    ('I', 'INCOME'),
+    ('E', 'EXPENSE'),
+)
+
 # Create your models here.
 class Board(models.Model):
     name = models.CharField('Name', max_length=100, blank=False)
@@ -10,11 +15,20 @@ class Board(models.Model):
     description = models.TextField()
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
-        super().save(*args, **kwargs)
+        super(Board, self).save(*args, **kwargs)
+
+class Category(models.Model):
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    name  = models.CharField(max_length=50, blank=False)
+    description = models.TextField()
 
 class Transaction(models.Model):
     """
     Transactions model
     It  manages the transactions of company to different users
     """
-    pass
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    title = models.CharField('Title', max_length=100)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    transaction_type = models.CharField(max_length=1, choices=TRANSACTION_TYPES)
