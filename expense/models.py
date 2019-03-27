74d2eb41ed1d1613dd7bfb366c377490cacf9591
Zero Cool <shahzaibakash@gmail.com>
2019-03-27 17:29:49 +0500

    removed transaction_type from Expense Model

--- expense/models.py
+++ expense/models.py
@@ -3,10 +3,6 @@ from django.contrib.auth import get_user_model
 from django.utils.text import slugify
 from django.utils import timezone
 
-TRANSACTION_TYPES = (
-    ('I', 'INCOME'),
-    ('E', 'EXPENSE'),
-)
 
 # Create your models here.
 class Board(models.Model):
@@ -44,7 +40,6 @@ class Expense(models.Model):
     title = models.CharField('Title', max_length=100)
     amount = models.DecimalField(max_digits=8, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    transaction_type = models.CharField(max_length=1, choices=TRANSACTION_TYPES)
     transaction_date = models.DateField('Transaction Date', default=timezone.now)
     created_at = models.DateTimeField(default=timezone.now, editable=False)
     updated_at = models.DateTimeField(auto_now=True)
